2e782437ddd92d0a1f64d75883e7dd9225317d07
rikarika57 <morikawarika.57@gmail.com>
2016-06-20 13:58:59 +0900

    Add files via upload

---
+++ common.php
@@ -0,0 +1,7 @@
+<?php
+
+//common.php
+
+define( 'CONSUMER_KEY', '2d6cX9EOvbvfJXsh1au7hA' );
+define( 'CONSUMER_SECRET', 'P5GQZd62QNpZmm4OD5T5GOQfocqPgbRTzOsxJYq4CY' );
+define( 'OAUTH_CALLBACK', 'http://gms.gdl.jp/~mk57/aicotoba/callback.php' );
