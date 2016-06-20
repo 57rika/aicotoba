6fae6b5a14fe74f42da5b31030d77d7e6214107c
rikarika57 <morikawarika.57@gmail.com>
2016-06-20 15:30:05 +0900

    Update common.php

--- common.php
+++ common.php
@@ -2,6 +2,6 @@
 
 //common.php
 
-define( 'CONSUMER_KEY', '2d6cX9EOvbvfJXsh1au7hA' );
-define( 'CONSUMER_SECRET', 'P5GQZd62QNpZmm4OD5T5GOQfocqPgbRTzOsxJYq4CY' );
+define( 'CONSUMER_KEY', 'OoYsFqcPVrMx24RHYeV3EMTXC' );
+define( 'CONSUMER_SECRET', 'ATfWC436rEhxxlOSPT1YjfCkR0lm16bkMX1fk01KUDua8GCrYu' );
 define( 'OAUTH_CALLBACK', 'http://gms.gdl.jp/~mk57/aicotoba/callback.php' );
