b681396fe7165060738d358e53c2b7dfacc7421e
rikarika57 <morikawarika.57@gmail.com>
2016-06-20 17:40:25 +0900

    Add files via upload

--- result.php
+++ result.php
@@ -1,4 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta name=”keywords” content=”Aicotoba,診断,カップル,ラブラブ度”>
+<meta name=”keywords” content="何気ないメールのやり取りの中からAicotoba&quot;愛言葉&quot;を見つけるアプリ">
+<meta name="viewport" content="width=device-width,user-scalable=yes">
+<title>friend list</title>
+</head>
+<body>
+<script type="text/javascript">
+	(function (){
+		var snow =["images/heart_smaill_gold.png","images/heart_small_pink.png"];
+		var Zx="-1";
+		var num=25;
+		var imgw=34;
+		var imgh=34;
+		var spd=70;
+		var maxOp=0.9;
+		var miniOp=0.4;
+
+		var sx=[], sy=[], sp=[], opa=[];
+		var len=snow.length, i=0;
+		var w=window.innerWidth+imgw, h=window.innerHeight+imgh;
+		for (i=0; i<num; i++){
+			sx[i]=Math.floor(Math.random()*w);
+			sy[i]=Math.floor(Math.random()*h);
+			sp[i]=Math.floor(Math.random()*6)+2;
+			opa[i]=Math.random()*(maxOp-miniOp)+miniOp;
+			document.write("<img src="+snow[i%len]+" id='sn"+i+"'>");
+		}
+		function moveSnow(){
+			for (i=0; i<num; i++){
+				sy[i] += sp[i];
+				if (sy[i] > h) sy[i] = -imgh;
+				var ob=document.getElementById("sn"+i).style;
+				ob.top=-imgh+sy[i]+"px"; ob.left=-imgw+sx[i]+"px";
+				ob.position="fixed"; ob.zIndex=Zx; ob.opacity=opa[i];
+			}
+			setTimeout(moveSnow,spd);
+		}
+		moveSnow();
+	}());
+</script>
+
+	<header>
+		<div class="header_wrapper">
+			<h1>Aicotoba</h1><span>何気ないメールのやり取りの中から"愛言葉"を見つけるアプリ</span>
+		</div>
+	</header>
+	<?php
 
 session_start();
 
@@ -27,14 +80,15 @@ if(isset($_POST['tmes'])){
 $messages = $connection->OAuthRequest("https://api.twitter.com/1.1/direct_messages.json","GET",array('count'=>'200'));
 //$array = json_decode( $messages , true ) ;
 $omsg = json_decode( $messages ) ;
-
 if (isset($tmes)){
   /* ターゲットが決まってる時の処理、診断 */
 	if (isset($omsg) && empty($omsf->errors)) {
-	echo '<dl>';
 	foreach ($omsg as $val) {
+		$twuname=$val->recipient->name;
+
 		if ($val->sender->screen_name ==$tmes){
-				$dtmp=date('Y-m-d', strtotime($val->created_at));
+				$twfname=$val->sender->name;
+				$dtmp=date('Y年m月d日', strtotime($val->created_at));
 				$ical[$dtmp]=$ical[$dtmp]+1;
 /* mecab */
 		$nodes = $mecab->parseToNode($val->text);
@@ -56,7 +110,7 @@ if (isset($tmes)){
     $array = json_decode( $sents, true ) ;
     foreach ($imsg as $val) {
 	if ($val->recipient->screen_name ==$tmes){
-				$dtmp=date('Y-m-d', strtotime($val->created_at));
+				$dtmp=date('Y年m月d日', strtotime($val->created_at));
 				$ocal[$dtmp]=$ocal[$dtmp]+1;
 
 	    $nodes = $mecab->parseToNode($val->text);
@@ -92,31 +146,113 @@ echo $n->getFeature() . "<br />";
 		if ($value>=$maxout){$maxout=$value;$maxoutd=$key;}
    }
    $avout=$oall/$i;
-   echo 'avgin:' . $avin . " avgout:" . $avout .  " maxindate:" . $maxind . " maxoutdate:" . $maxoutd . "<br>";
+   $lovedo= $ocal[$maxoutd]/$ical[$maxoutd] *100;
 
-	echo '<h1>From Partner</h1>';
-	foreach ($nwords as $key =>$value){
-	    echo $key . ': ' . $value . '<br>';
-	}
+	echo '<main>';
+		echo '<div class="eye_catch">';
+			echo '<div class="wrapper">';
+				echo '<p class="result_title"><span class="aicotoba">' . $twuname . '</span>さんの診断結果</p>';
+				echo '<div class="heart">';
+					echo '<p>' . $twfname . 'とのAicotoba度は</p>';
+					echo '<p><span>' . (int)$lovedo . '%</span><span>です。</span></p>';
+				echo '</div>';
+				echo '<div class="details">';
+					echo '<p><span>一日平均</span><span id="oneday_send">' . (int)$avout . '通</span><span>のメールを送りました。</span></p>';
+					echo '<p><span>一日平均</span><span id="oneday_receive">' . (int)$avin . '通</span><span>のメールが届きました。</span></p>';
+					echo '<p><span>最もメールを送った日は</span><span id="most_send">' . $maxoutd . '</span><span>です。</span></p>';
+					echo '<p><span>最もメールを受け取った日は</span><span id="most_receive">' . $maxind . '</span><span>です。</span></p>';
+				echo '</div>';
+			echo '</div>';
+		echo '</div>';
+		echo '<div class="my_aicotoba">';
+			echo '<div class="wrapper">';
+				echo '<h2><img src="images/leaf01.png">あなたの<span class="aicotoba">Aicotoba</span><img src="images/leaf02.png"></h2>';
+				echo '<div class="word">';
+				foreach ($iwords as $key =>$value){
+//		echo $key . ': ' . $value . '<br>';
+					$size=$value*14;
+					echo '<p style="position: absolute; top: ' . rand(60,440) . 'px; left: ' . rand(60,1000) . 'px; font-size: ' . $size . 'px; font-family: Angelina,Hannari;">' . $key . '</p>';
+				}
+				echo '</div>';
+			echo '</div>';
+		echo '</div>';
+		echo '<div class="lovers_aicotoba">';
+			echo '<div class="wrapper">';
+				echo '<h2><img src="images/leaf01.png"><span>' .$twfname .'</span>の<span class="aicotoba">Aicotoba</span><img src="images/leaf02.png"></h2>';
+				echo '<div class="word">';
+					foreach ($nwords as $key =>$value){
+	   			$size=$value*14;
+	   				echo '<p style="position: absolute; top: ' . rand(60,440) . 'px; left: ' . rand(60,1000) . 'px; font-size: ' . $size . 'px; font-family: Angelina,Hannari;">' . $key . '</p>';
 
-	echo '<h1>To Partner</h1>';
-    foreach ($iwords as $key =>$value){
-	echo $key . ': ' . $value . '<br>';
-    }
+//	    echo $key . ': ' . $value . '<br>';
+					}
+
+
+				echo '</div>';
+			echo '</div>';
+		echo '</div>';
+		echo '<div class="message">';
+			echo '<div class="wrapper">';
+				echo '<p>いかがでしたか？<br>"Aicotoba"はたくさん見つかりましたでしょうか？<br>たくさん見つけた方も、そうでなかった方も、<br>思いやりや優しさを込めてコトバを贈っていきましょう。</p>';
+			echo '</div>';
+		echo '</div>';
+		echo '<div class="share">';
+			echo '<div class="wrapper">';
+				echo '<h2>ふたりの<span class="aicotoba">Aicotoba</span>を…</h2>';
+				echo '<div class="twitter"><a href=""><img src="images/twitter.svg" width="30" height="30"><span>ツイートする</span></a></div>';
+				echo '<div class="facebook"><a href=""><img src="images/facebook.svg" width="30" height="30"><span>シェアする</span></a></div>';
+			echo '</div>';
+		echo '</div>';
+	echo '</main>';
+
+//kokokara
+//   echo 'avgin:' . $avin . " avgout:" . $avout .  " maxindate:" . $maxind . " maxoutdate:" . $maxoutd . "<br>";
+
+//	echo '<h1>From Partner</h1>';
+//	foreach ($nwords as $key =>$value){
+//	    echo $key . ': ' . $value . '<br>';
+//	}
+
+//	echo '<h1>To Partner</h1>';
+ //   foreach ($iwords as $key =>$value){
+//		echo $key . ': ' . $value . '<br>';
+ //   }
+//kokomade
+
 }
 else{
-    echo '<form method="POST" action="./result.php">';
-    /* ユーザリストから対象選択 */
-    if (isset($omsg) && empty($omsf->errors)) {
-	echo '<dl>';
-	foreach ($omsg as $val) { 
-	    $slist[$val->sender->screen_name][0]=$val->sender->name ;
-	    $slist[$val->sender->screen_name][1]=$val->sender->profile_image_url_https ;
-	}
-	foreach ($slist as  $key => $value){
-	    echo $value[1] . ',' . $value[0] . '<input type="radio" name="tmes" value="' . $key . '"><br>';
-	}
-	echo '<input type="submit"></form>';
+	echo '<main>';
+		echo '<div class="friend_list">';
+			echo '<div class="wrapper">';
+				echo '<p>Aicotobaを探したいお相手をお選びください。</p>';
+				echo '<div class="list">';
+					echo '<table>';
+
+					    echo '<form method="POST" action="./result.php">';
+					    /* ユーザリストから対象選択 */
+					    if (isset($omsg) && empty($omsf->errors)) {
+						foreach ($omsg as $val) {
+						    $slist[$val->sender->screen_name][0]=$val->sender->name ;
+						    $slist[$val->sender->screen_name][1]=$val->sender->profile_image_url_https ;
+						}
+						foreach ($slist as  $key => $value){
+
+							echo '<tr>';
+						    echo '<td><img src="'. $value[1] . '"></td>';
+						 	echo '<td>' . $value[0] . '</td>';
+					    	echo '<td><input type="radio" name="tmes" value="' . $key . '"></td>';
+						  	echo '</tr>';
+
+						}
+
+
+				echo '</table>';
+				echo '</div>';
+//				echo '<a href="" class="btn_start">Start</a>';
+				echo '<input type="submit" value ="Start" class="btn_start"></form>';
+			echo '</div>';
+		echo '</div>';
+	echo '</main>';
     } else {
     }
 }
@@ -125,5 +261,12 @@ else{
 //print_r( $sents);
 // print_r($ical);
 // print_r($ocal);
- ?>
+?>
 
+	<footer>
+		<div class="wrapper">
+			<small><p>Copyright © Aicokotoba All Rights Reserved.</p></small>
+		</div>
+	</footer>
+</body>
+</html>
